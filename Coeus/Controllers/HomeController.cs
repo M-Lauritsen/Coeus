4ca5041ba4fb6f3c53cb8ab2ab2835c78c1d7626
Michael Lauritsen <i30288741@live.dk>
2019-07-03 16:25:28 +0200

    tested/created views

--- Coeus/Controllers/HomeController.cs
+++ Coeus/Controllers/HomeController.cs
@@ -15,10 +15,15 @@ namespace Coeus.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
